755a212fd55f13ac846b4d9be5596e847a248acd
Henrique <hentt30@gmail.com>
2020-02-24 15:38:26 -0500

    adding presentation text

--- landing-page/src/LandingPage/QuemSomos/QuemSomos.js
+++ landing-page/src/LandingPage/QuemSomos/QuemSomos.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Grid from "@material-ui/core/Grid";
 import styled from "styled-components";
+import Typography from "@material-ui/core/Typography";
 
 const Wrapper = styled.div`
   display: flex;
@@ -14,19 +15,31 @@ const StyledGrid = styled(Grid)`
   justify-content: center;
 `;
 
+const StyledTypography = styled(Typography)`
+  font-family: lato;
+  color: white;
+  font-size: 150%;
+  padding: 40px;
+`;
 export default function QuemSomos() {
   return (
     <Wrapper>
       <Grid container direction="row" alignItems="center">
         <StyledGrid item xs>
           <img
-            style={{ width: "65vw", height: "100vh" }}
-            src={window.location.origin + "/coffee.jpg"}
+            style={{ width: "45vw", height: "45vh" }}
+            src={window.location.origin + "/food.png"}
             alt="imagem qualquer"
           />
         </StyledGrid>
         <StyledGrid item xs display="flex" justifyContent="center">
-          Arquivo JS do texto e tals
+          <StyledTypography>
+            Coffee apps é uma software house criada no alojamento do ITA.Nosso
+            maior objetivo é realizar projetos de aplicativos . Agora basta
+            gerar um texto aleatório que será preenchido futuramente por
+            palavras sem sentido que irão convencer outras pessoas a comprarem
+            nosso produto
+          </StyledTypography>
         </StyledGrid>
       </Grid>
     </Wrapper>
