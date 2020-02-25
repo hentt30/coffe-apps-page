438bd48a0933a6b393a6815c4516a1df6663eb23
Henrique <hentt30@gmail.com>
2020-02-25 18:00:04 -0500

    Add app bar

--- landing-page/src/LandingPage/NavBar/NavBar.js
+++ landing-page/src/LandingPage/NavBar/NavBar.js
@@ -1,21 +1,60 @@
 import React from "react";
-import { makeStyles } from "@material-ui/core/styles";
+import { fade, makeStyles } from "@material-ui/core/styles";
 import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
-import Typography from "@material-ui/core/Typography";
 import Button from "@material-ui/core/Button";
-import IconButton from "@material-ui/core/IconButton";
-import MenuIcon from "@material-ui/icons/Menu";
+import LocalCafeIcon from "@material-ui/icons/LocalCafe";
+import Grid from "@material-ui/core/Grid";
+import SearchIcon from "@material-ui/icons/Search";
+import InputBase from "@material-ui/core/InputBase";
 
 const useStyles = makeStyles(theme => ({
   root: {
-    flexGrow: 1
+    flexGrow: 1,
+    fontFamily: "lato"
   },
   menuButton: {
     marginRight: theme.spacing(2)
   },
   title: {
     flexGrow: 1
+  },
+  search: {
+    position: "relative",
+    borderRadius: theme.shape.borderRadius,
+    backgroundColor: fade(theme.palette.common.white, 0.15),
+    "&:hover": {
+      backgroundColor: fade(theme.palette.common.white, 0.25)
+    },
+    marginLeft: 0,
+    width: "100%",
+    [theme.breakpoints.up("sm")]: {
+      marginLeft: theme.spacing(1),
+      width: "auto"
+    }
+  },
+  searchIcon: {
+    width: theme.spacing(7),
+    height: "100%",
+    position: "absolute",
+    pointerEvents: "none",
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center"
+  },
+  inputRoot: {
+    color: "inherit"
+  },
+  inputInput: {
+    padding: theme.spacing(1, 1, 1, 7),
+    transition: theme.transitions.create("width"),
+    width: "100%",
+    [theme.breakpoints.up("sm")]: {
+      width: 120,
+      "&:focus": {
+        width: 200
+      }
+    }
   }
 }));
 
@@ -24,8 +63,40 @@ export default function ButtonAppBar() {
 
   return (
     <div className={classes.root}>
-      <AppBar position="fixed">
-        <Toolbar></Toolbar>
+      <AppBar position="fixed" color="secondary">
+        <Toolbar>
+          <Grid item xs={1}>
+            <LocalCafeIcon position="start" fontSize="large" />
+          </Grid>
+          <Grid item xs={1}>
+            <Button color="inherit">Home</Button>
+          </Grid>
+          <Grid item xs={1}>
+            <Button color="inherit">Sobre</Button>
+          </Grid>
+          <Grid item xs={1}>
+            <Button color="inherit">Projetos</Button>
+          </Grid>
+          <Grid item xs={1}>
+            <Button color="inherit">Contato</Button>
+          </Grid>
+          <Grid item xs={4}></Grid>
+          <Grid item xs={3}>
+            <div className={classes.search}>
+              <div className={classes.searchIcon}>
+                <SearchIcon />
+              </div>
+              <InputBase
+                placeholder="Search…"
+                classes={{
+                  root: classes.inputRoot,
+                  input: classes.inputInput
+                }}
+                inputProps={{ "aria-label": "search" }}
+              />
+            </div>
+          </Grid>
+        </Toolbar>
       </AppBar>
     </div>
   );
--- landing-page/src/LandingPage/Projetos/Projetos.js
+++ landing-page/src/LandingPage/Projetos/Projetos.js
@@ -5,75 +5,85 @@ import styled from "styled-components";
 import MediaCard from "./Card/Card";
 
 const Wrapper = styled.div`
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    justify-content: space-evenly;
-    min-height: 100vh;
-    min-width: 100vw;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-evenly;
+  min-height: 100vh;
+  min-width: 100vw;
 `;
 
 const StyledGrid = styled(Grid)`
-    max-width: 384px;
-    padding: 10px 0px 10px;
+  max-width: 384px;
+  padding: 10px 0px 10px;
 `;
 
-
 export default function Projetos() {
-    const DataCard = [
-        {
-            img: "",
-            title: "Lorem Ipsum",
-            subtitle: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis gravida sem justo, eget sodales metus dignissim vel.",
-            description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis gravida sem justo, eget sodales metus dignissim vel. Vestibulum porttitor magna non odio vulputate interdum. Suspendisse egestas eu purus nec ultricies. Vivamus in aliquam quam. Donec consequat tellus convallis, scelerisque nisi ut, condimentum mi. Pellentesque interdum, orci eu gravida consequat, odio erat vestibulum orci, congue consectetur odio urna id enim. Aenean in efficitur massa. Duis dignissim a ipsum sed dapibus. Phasellus sem diam, pulvinar at bibendum in, rhoncus lacinia dui. Aliquam ac lobortis quam. Quisque in nisi elit. Proin at tortor est."
-        },
-        {
-            img: "",
-            title: "Lorem Ipsum",
-            subtitle: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis gravida sem justo, eget sodales metus dignissim vel.",
-            description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis gravida sem justo, eget sodales metus dignissim vel. Vestibulum porttitor magna non odio vulputate interdum. Suspendisse egestas eu purus nec ultricies. Vivamus in aliquam quam. Donec consequat tellus convallis, scelerisque nisi ut, condimentum mi. Pellentesque interdum, orci eu gravida consequat, odio erat vestibulum orci, congue consectetur odio urna id enim. Aenean in efficitur massa. Duis dignissim a ipsum sed dapibus. Phasellus sem diam, pulvinar at bibendum in, rhoncus lacinia dui. Aliquam ac lobortis quam. Quisque in nisi elit. Proin at tortor est."
-        },
-        {
-            img: "",
-            title: "Lorem Ipsum",
-            subtitle: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis gravida sem justo, eget sodales metus dignissim vel.",
-            description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis gravida sem justo, eget sodales metus dignissim vel. Vestibulum porttitor magna non odio vulputate interdum. Suspendisse egestas eu purus nec ultricies. Vivamus in aliquam quam. Donec consequat tellus convallis, scelerisque nisi ut, condimentum mi. Pellentesque interdum, orci eu gravida consequat, odio erat vestibulum orci, congue consectetur odio urna id enim. Aenean in efficitur massa. Duis dignissim a ipsum sed dapibus. Phasellus sem diam, pulvinar at bibendum in, rhoncus lacinia dui. Aliquam ac lobortis quam. Quisque in nisi elit. Proin at tortor est."
-        },
-        {
-            img: "",
-            title: "Lorem Ipsum",
-            subtitle: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis gravida sem justo, eget sodales metus dignissim vel.",
-            description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis gravida sem justo, eget sodales metus dignissim vel. Vestibulum porttitor magna non odio vulputate interdum. Suspendisse egestas eu purus nec ultricies. Vivamus in aliquam quam. Donec consequat tellus convallis, scelerisque nisi ut, condimentum mi. Pellentesque interdum, orci eu gravida consequat, odio erat vestibulum orci, congue consectetur odio urna id enim. Aenean in efficitur massa. Duis dignissim a ipsum sed dapibus. Phasellus sem diam, pulvinar at bibendum in, rhoncus lacinia dui. Aliquam ac lobortis quam. Quisque in nisi elit. Proin at tortor est."
-        },
-        {
-            img: "",
-            title: "Lorem Ipsum",
-            subtitle: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis gravida sem justo, eget sodales metus dignissim vel.",
-            description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis gravida sem justo, eget sodales metus dignissim vel. Vestibulum porttitor magna non odio vulputate interdum. Suspendisse egestas eu purus nec ultricies. Vivamus in aliquam quam. Donec consequat tellus convallis, scelerisque nisi ut, condimentum mi. Pellentesque interdum, orci eu gravida consequat, odio erat vestibulum orci, congue consectetur odio urna id enim. Aenean in efficitur massa. Duis dignissim a ipsum sed dapibus. Phasellus sem diam, pulvinar at bibendum in, rhoncus lacinia dui. Aliquam ac lobortis quam. Quisque in nisi elit. Proin at tortor est."
-        },
-        {
-            img: "",
-            title: "Lorem Ipsum",
-            subtitle: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis gravida sem justo, eget sodales metus dignissim vel.",
-            description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis gravida sem justo, eget sodales metus dignissim vel. Vestibulum porttitor magna non odio vulputate interdum. Suspendisse egestas eu purus nec ultricies. Vivamus in aliquam quam. Donec consequat tellus convallis, scelerisque nisi ut, condimentum mi. Pellentesque interdum, orci eu gravida consequat, odio erat vestibulum orci, congue consectetur odio urna id enim. Aenean in efficitur massa. Duis dignissim a ipsum sed dapibus. Phasellus sem diam, pulvinar at bibendum in, rhoncus lacinia dui. Aliquam ac lobortis quam. Quisque in nisi elit. Proin at tortor est."
-        },
+  const DataCard = [
+    {
+      img: "",
+      title: "Lorem Ipsum",
+      subtitle:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis gravida sem justo, eget sodales metus dignissim vel.",
+      description:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis gravida sem justo, eget sodales metus dignissim vel. Vestibulum porttitor magna non odio vulputate interdum. Suspendisse egestas eu purus nec ultricies. Vivamus in aliquam quam. Donec consequat tellus convallis, scelerisque nisi ut, condimentum mi. Pellentesque interdum, orci eu gravida consequat, odio erat vestibulum orci, congue consectetur odio urna id enim. Aenean in efficitur massa. Duis dignissim a ipsum sed dapibus. Phasellus sem diam, pulvinar at bibendum in, rhoncus lacinia dui. Aliquam ac lobortis quam. Quisque in nisi elit. Proin at tortor est."
+    },
+    {
+      img: "",
+      title: "Lorem Ipsum",
+      subtitle:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis gravida sem justo, eget sodales metus dignissim vel.",
+      description:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis gravida sem justo, eget sodales metus dignissim vel. Vestibulum porttitor magna non odio vulputate interdum. Suspendisse egestas eu purus nec ultricies. Vivamus in aliquam quam. Donec consequat tellus convallis, scelerisque nisi ut, condimentum mi. Pellentesque interdum, orci eu gravida consequat, odio erat vestibulum orci, congue consectetur odio urna id enim. Aenean in efficitur massa. Duis dignissim a ipsum sed dapibus. Phasellus sem diam, pulvinar at bibendum in, rhoncus lacinia dui. Aliquam ac lobortis quam. Quisque in nisi elit. Proin at tortor est."
+    },
+    {
+      img: "",
+      title: "Lorem Ipsum",
+      subtitle:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis gravida sem justo, eget sodales metus dignissim vel.",
+      description:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis gravida sem justo, eget sodales metus dignissim vel. Vestibulum porttitor magna non odio vulputate interdum. Suspendisse egestas eu purus nec ultricies. Vivamus in aliquam quam. Donec consequat tellus convallis, scelerisque nisi ut, condimentum mi. Pellentesque interdum, orci eu gravida consequat, odio erat vestibulum orci, congue consectetur odio urna id enim. Aenean in efficitur massa. Duis dignissim a ipsum sed dapibus. Phasellus sem diam, pulvinar at bibendum in, rhoncus lacinia dui. Aliquam ac lobortis quam. Quisque in nisi elit. Proin at tortor est."
+    },
+    {
+      img: "",
+      title: "Lorem Ipsum",
+      subtitle:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis gravida sem justo, eget sodales metus dignissim vel.",
+      description:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis gravida sem justo, eget sodales metus dignissim vel. Vestibulum porttitor magna non odio vulputate interdum. Suspendisse egestas eu purus nec ultricies. Vivamus in aliquam quam. Donec consequat tellus convallis, scelerisque nisi ut, condimentum mi. Pellentesque interdum, orci eu gravida consequat, odio erat vestibulum orci, congue consectetur odio urna id enim. Aenean in efficitur massa. Duis dignissim a ipsum sed dapibus. Phasellus sem diam, pulvinar at bibendum in, rhoncus lacinia dui. Aliquam ac lobortis quam. Quisque in nisi elit. Proin at tortor est."
+    },
+    {
+      img: "",
+      title: "Lorem Ipsum",
+      subtitle:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis gravida sem justo, eget sodales metus dignissim vel.",
+      description:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis gravida sem justo, eget sodales metus dignissim vel. Vestibulum porttitor magna non odio vulputate interdum. Suspendisse egestas eu purus nec ultricies. Vivamus in aliquam quam. Donec consequat tellus convallis, scelerisque nisi ut, condimentum mi. Pellentesque interdum, orci eu gravida consequat, odio erat vestibulum orci, congue consectetur odio urna id enim. Aenean in efficitur massa. Duis dignissim a ipsum sed dapibus. Phasellus sem diam, pulvinar at bibendum in, rhoncus lacinia dui. Aliquam ac lobortis quam. Quisque in nisi elit. Proin at tortor est."
+    },
+    {
+      img: "",
+      title: "Lorem Ipsum",
+      subtitle:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis gravida sem justo, eget sodales metus dignissim vel.",
+      description:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis gravida sem justo, eget sodales metus dignissim vel. Vestibulum porttitor magna non odio vulputate interdum. Suspendisse egestas eu purus nec ultricies. Vivamus in aliquam quam. Donec consequat tellus convallis, scelerisque nisi ut, condimentum mi. Pellentesque interdum, orci eu gravida consequat, odio erat vestibulum orci, congue consectetur odio urna id enim. Aenean in efficitur massa. Duis dignissim a ipsum sed dapibus. Phasellus sem diam, pulvinar at bibendum in, rhoncus lacinia dui. Aliquam ac lobortis quam. Quisque in nisi elit. Proin at tortor est."
+    }
+  ];
+  // eslint-disable-next-line
+  const lin = 0;
 
-    ];
-
-    const lin = 0;
-
-    return (
-        <Wrapper>
-            <Grid container justify="center" alignItems="center">
-                <Typography variant="h2">Projetos</Typography>
-            </Grid>
-            <Grid container display="flex" alignItems="center" justify="space-evenly">
-                {DataCard.map(data => (
-                     <StyledGrid item>
-                        <MediaCard {...data}/>
-                    </StyledGrid>
-                ))}
-            </Grid>
-        </Wrapper>
-    );
-};
+  return (
+    <Wrapper>
+      <Grid container justify="center" alignItems="center">
+        <Typography variant="h2">Projetos</Typography>
+      </Grid>
+      <Grid container display="flex" alignItems="center" justify="space-evenly">
+        {DataCard.map(data => (
+          <StyledGrid item>
+            <MediaCard {...data} />
+          </StyledGrid>
+        ))}
+      </Grid>
+    </Wrapper>
+  );
+}
