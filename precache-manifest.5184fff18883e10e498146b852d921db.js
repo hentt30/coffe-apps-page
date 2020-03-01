9a323947fdb5bac00ab69098d832e97e84dca6b2
Henrique <hentt30@gmail.com>
2020-03-01 17:00:29 -0500

    Updates

---
+++ precache-manifest.5184fff18883e10e498146b852d921db.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "afb7029909bef6e2ff16145219d65900",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2b0ea974a31ba98fb0aa",
+    "url": "/static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "2b0ea974a31ba98fb0aa",
+    "url": "/static/js/2.a976b166.chunk.js"
+  },
+  {
+    "revision": "ac9fac6c8e755a8efecd01981adb88e6",
+    "url": "/static/js/2.a976b166.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9d98200e6e99631f9862",
+    "url": "/static/js/main.c824d9a4.chunk.js"
+  },
+  {
+    "revision": "f339b48a710a58a34cc3",
+    "url": "/static/js/runtime-main.5821a2c8.js"
+  }
+]);
